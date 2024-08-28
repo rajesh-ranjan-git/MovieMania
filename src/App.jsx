7764d74460894f4f9b597a00ace7b25462d51aa2
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-08-28 20:18:52 +0530

    Initiated

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main>
+      <p className="text-5xl text-blue-800 font-makeba">MovieMania</p>
+    </main>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: { makeba: ["Makeba"], nunito: ["NunitoSansRegular"] },
+    },
+  },
+  plugins: [],
+};
